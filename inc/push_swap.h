b17f89f70ffadb08c82418a783bf6cd466fc7e47
Torato Kitahara <tkitahar@c3r6s6.42tokyo.jp>
2024-08-21 20:40:54 +0900

    header now

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,8 +6,40 @@
 /*   By: tkitahar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 16:20:01 by tkitahar          #+#    #+#             */
-/*   Updated: 2024/08/20 20:52:48 by tkitahar         ###   ########.fr       */
+/*   Updated: 2024/08/21 18:15:35 by tkitahar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
 
+# include <stdbool.h>
+# include <limits.h>
+# include "../libft/includes/libft.h"
+
+typedef struct s_stack_node
+{
+	int					nbr;
+	int					index;
+	int					push_cost;
+	bool				above_median;
+	bool				cheapest;
+	struct s_stack_node	*taget_node;
+	struct s_stack_node	*next;
+	struct s_stack_node	*prev;
+}	t_stack_node;
+
+int				error_syntax(char *str_n);
+int				erro_duplicate(t_stack_node *a, int n);
+void			free_stack(t_stack_node **stack);
+
+void			init_stack_a(t_stack_node *a, char **argv);
+void			**split(char *s, char *c);
+
+void			init_nodes_a(t_stack_node *a, t_stack_node *b);
+void			init_nodes_b(t_stack_node *a, t_stack_node *b);
+void			current_index(t_stack_node *stack);
+
+
+
+#endif
