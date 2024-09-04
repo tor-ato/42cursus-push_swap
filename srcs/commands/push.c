af979ae858e02a02c658e7c4e3c92003de5921cd
Torato Kitahara <tkitahar@c4r3s13.42tokyo.jp>
2024-09-04 19:38:53 +0900

    sort_stacks left in comands

---
+++ srcs/commands/push.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkitahar <tkitahar@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 16:35:33 by tkitahar          #+#    #+#             */
+/*   Updated: 2024/09/04 19:17:21 by tkitahar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************* */
+
+#include "../../inc/push_swap.h"
+
+static void	push(t_stack_node **dst, t_stack_node **src)
+{
+	t_stack_node	*push_node;
+
+	if (!*src)
+		return ;
+	*src = (*src)->next;
+	if (*src)
+		(*src)->prev = NULL;
+	push_node->prev =NULL;
+	if (!*dst)
+	{
+		*dst = push_node;
+		push_node->next = NULL;
+	}
+	else
+	{
+		push_node->next = *dst;
+		push_node->next->prev = push_node;
+		*dst = push_node;
+	}
+}
+
+void	pa(t_stack_node **a, t_stack_node **b, bool print)
+{
+	push(a, b);
+	if (!print)
+		ft_printf("pa\n");
+}
+
+void	pb(t_stack_node **b, t_stack_node **a, bool print)
+{
+	push(b, a);
+	if (!print)
+		ft_printf("pb\n");
+}
---
+++ srcs/commands/rev_rotate.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rev_rotate.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkitahar <tkitahar@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 16:22:42 by tkitahar          #+#    #+#             */
+/*   Updated: 2024/09/04 16:34:33 by tkitahar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/push_swap.h"
+
+static void	rev_rotate(t_stack_node **stack)
+{
+	t_stack_node	*last;
+
+	if (!*stack || !(*stack)->next)
+		return ;
+	last = find_last(*stack);
+	last->prev->next = NULL;
+	last->next = *stack;
+	last->prev = NULL;
+	last->next->prev = last;
+}
+
+void	rra(t_stack_node **a, bool print)
+{
+	rev_rotate(a);
+	if (!print)
+		ft_printf("rra\n");
+}
+
+void	rrb(t_stack_node **b, bool print)
+{
+	rev_rotate(b);
+	if (!print)
+		ft_printf("rrb\n");
+}
+
+void	rrr(t_stack_node **a, t_stack_node **b, bool print)
+{
+	rev_rotate(a);
+	rev_rotate(b);
+	if (!print)
+		ft_printf("rrr\n");
+}
